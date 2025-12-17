125d75083b70e231dadba1c9e8031c544a0e9519
Lucas santos <119458256+Lucas-santos-35@users.noreply.github.com>
2025-12-17 18:34:15 -0300

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,17 +1,17 @@
 {
-  "short_name": "Dream Hub",
-  "name": "Lucas Dream Hub - Planeamento",
-  "icons": [
-    {
-      "src": "https://cdn-icons-png.flaticon.com/512/2906/2906323.png",
-      "type": "image/png",
-      "sizes": "512x512"
-    }
-  ],
-  "start_url": "./index.html",
-  "background_color": "#ffffff",
-  "display": "standalone",
-  "scope": "./",
-  "theme_color": "#0f172a",
-  "description": "App de planeamento financeiro para Lucas & Amada."
+"name": "Lucas Dream Hub",
+"short_name": "DreamHub",
+"description": "Planeamento de metas para Lucas & Amada",
+"start_url": "index.html",
+"display": "standalone",
+"background_color": "#020617",
+"theme_color": "#0f172a",
+"icons": [
+{
+"src": "https://cdn-icons-png.flaticon.com/512/2906/2906323.png",
+"sizes": "512x512",
+"type": "image/png",
+"purpose": "any maskable"
+}
+]
 }
