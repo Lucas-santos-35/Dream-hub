f4e373b5cf11ed1def3b0521bfb4ecf1ee285ac4
Lucas santos <119458256+Lucas-santos-35@users.noreply.github.com>
2025-12-17 17:43:43 -0300

    Create manifest.js for Dream Hub
    Add manifest file for the Dream Hub application

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+  "short_name": "Dream Hub",
+  "name": "Lucas Dream Hub - Planeamento",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2906/2906323.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "./index.html",
+  "background_color": "#ffffff",
+  "display": "standalone",
+  "scope": "./",
+  "theme_color": "#0f172a",
+  "description": "App de planeamento financeiro para Lucas & Amada."
+}
