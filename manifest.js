e08b8a6a884f3e81ddcbdc40cadd4e07adab81ac
Lucas santos <119458256+Lucas-santos-35@users.noreply.github.com>
2025-12-19 11:13:03 -0300

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,17 +1,20 @@
 {
-"name": "Lucas Dream Hub",
-"short_name": "DreamHub",
-"description": "Planeamento de metas para Lucas & Amada",
-"start_url": "index.html",
-"display": "standalone",
-"background_color": "#020617",
-"theme_color": "#0f172a",
-"icons": [
-{
-"src": "https://cdn-icons-png.flaticon.com/512/2906/2906323.png",
-"sizes": "512x512",
-"type": "image/png",
-"purpose": "any maskable"
-}
-]
+  "name": "Dream Hub Mobile",
+  "short_name": "DreamHub",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#070a13",
+  "theme_color": "#002d5e",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/5551/5551323.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/5551/5551323.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
 }
